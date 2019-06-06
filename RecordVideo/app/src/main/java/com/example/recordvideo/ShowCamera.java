67df281c44b908fb3e148760e65aea4410f09649
Kamran Janjua <kamranejaz98@gmail.com>
2019-06-06 12:49:17 +0300

    Added the recording video and saving part

---
+++ RecordVideo/app/src/main/java/com/example/recordvideo/ShowCamera.java
@@ -0,0 +1,52 @@
+package com.example.recordvideo;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import java.util.List;
+import java.io.IOException;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+    Camera camera;
+    SurfaceHolder holder;
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Camera.Parameters params = camera.getParameters();
+        List<Camera.Size> sizeList = params.getSupportedPictureSizes();
+        Camera.Size mSize = sizeList.get(sizeList.size() - 1);
+        if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
+            params.set("orientation", "portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }else{
+            params.set("orientation", "landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+        params.setPictureSize(mSize.width, mSize.height);
+        camera.setParameters(params);
+        try{
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {}
+
+}
